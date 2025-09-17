085ce353d064a4ef4aafb672b0059fd3f8e570bb
Antonio Martínez <antonio.martinez@roto-frank.com>
2025-09-17 15:04:45 +0200

    Cambios en diseño para adaptación a zoom 125%

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -47,10 +47,9 @@
             // 
             btn_LoadXml.BackgroundImage = (Image)resources.GetObject("btn_LoadXml.BackgroundImage");
             btn_LoadXml.BackgroundImageLayout = ImageLayout.Stretch;
-            btn_LoadXml.Location = new Point(47, 54);
-            btn_LoadXml.Margin = new Padding(3, 2, 3, 2);
+            btn_LoadXml.Location = new Point(42, 72);
             btn_LoadXml.Name = "btn_LoadXml";
-            btn_LoadXml.Size = new Size(40, 42);
+            btn_LoadXml.Size = new Size(58, 56);
             btn_LoadXml.TabIndex = 0;
             btn_LoadXml.UseVisualStyleBackColor = true;
             btn_LoadXml.Click += btn_LoadXml_Click;
@@ -59,9 +58,9 @@
             // 
             lbl_Xml.BackColor = Color.Transparent;
             lbl_Xml.Font = new Font("Segoe UI", 8F);
-            lbl_Xml.Location = new Point(93, 68);
+            lbl_Xml.Location = new Point(106, 91);
             lbl_Xml.Name = "lbl_Xml";
-            lbl_Xml.Size = new Size(561, 28);
+            lbl_Xml.Size = new Size(586, 46);
             lbl_Xml.TabIndex = 1;
             lbl_Xml.Text = "Seleccionar XML";
             // 
@@ -70,10 +69,9 @@
             statusStrip1.BackColor = Color.Transparent;
             statusStrip1.ImageScalingSize = new Size(20, 20);
             statusStrip1.Items.AddRange(new ToolStripItem[] { lbl_Conexion });
-            statusStrip1.Location = new Point(0, 332);
+            statusStrip1.Location = new Point(0, 446);
             statusStrip1.Name = "statusStrip1";
-            statusStrip1.Padding = new Padding(1, 0, 12, 0);
-            statusStrip1.Size = new Size(683, 22);
+            statusStrip1.Size = new Size(781, 26);
             statusStrip1.SizingGrip = false;
             statusStrip1.TabIndex = 2;
             statusStrip1.Text = "statusStrip1";
@@ -81,17 +79,16 @@
             // lbl_Conexion
             // 
             lbl_Conexion.Name = "lbl_Conexion";
-            lbl_Conexion.Size = new Size(118, 17);
+            lbl_Conexion.Size = new Size(151, 20);
             lbl_Conexion.Text = "toolStripStatusLabel1";
             // 
             // cmb_Conectores
             // 
             cmb_Conectores.DropDownStyle = ComboBoxStyle.DropDownList;
             cmb_Conectores.FormattingEnabled = true;
-            cmb_Conectores.Location = new Point(218, 128);
-            cmb_Conectores.Margin = new Padding(3, 2, 3, 2);
+            cmb_Conectores.Location = new Point(308, 172);
             cmb_Conectores.Name = "cmb_Conectores";
-            cmb_Conectores.Size = new Size(163, 23);
+            cmb_Conectores.Size = new Size(186, 28);
             cmb_Conectores.TabIndex = 3;
             cmb_Conectores.SelectedValueChanged += cmb_Conectores_SelectedValueChanged;
             // 
@@ -99,10 +96,9 @@
             // 
             btn_SetsNoUtilizados.BackgroundImage = (Image)resources.GetObject("btn_SetsNoUtilizados.BackgroundImage");
             btn_SetsNoUtilizados.BackgroundImageLayout = ImageLayout.Stretch;
-            btn_SetsNoUtilizados.Location = new Point(47, 180);
-            btn_SetsNoUtilizados.Margin = new Padding(3, 2, 3, 2);
+            btn_SetsNoUtilizados.Location = new Point(42, 240);
             btn_SetsNoUtilizados.Name = "btn_SetsNoUtilizados";
-            btn_SetsNoUtilizados.Size = new Size(40, 40);
+            btn_SetsNoUtilizados.Size = new Size(58, 51);
             btn_SetsNoUtilizados.TabIndex = 4;
             btn_SetsNoUtilizados.UseVisualStyleBackColor = true;
             btn_SetsNoUtilizados.Click += btn_SetsNoUtilizados_Click;
@@ -111,9 +107,9 @@
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
-            label1.Location = new Point(93, 190);
+            label1.Location = new Point(106, 253);
             label1.Name = "label1";
-            label1.Size = new Size(91, 15);
+            label1.Size = new Size(116, 20);
             label1.TabIndex = 5;
             label1.Text = "Revisión de Sets";
             // 
@@ -121,9 +117,9 @@
             // 
             label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
-            label2.Location = new Point(93, 131);
+            label2.Location = new Point(106, 175);
             label2.Name = "label2";
-            label2.Size = new Size(119, 15);
+            label2.Size = new Size(149, 20);
             label2.TabIndex = 6;
             label2.Text = "Seleccionar Conector";
             // 
@@ -132,10 +128,9 @@
             btn_Actualizar.BackColor = Color.Transparent;
             btn_Actualizar.BackgroundImage = (Image)resources.GetObject("btn_Actualizar.BackgroundImage");
             btn_Actualizar.BackgroundImageLayout = ImageLayout.Stretch;
-            btn_Actualizar.Location = new Point(47, 118);
-            btn_Actualizar.Margin = new Padding(3, 2, 3, 2);
+            btn_Actualizar.Location = new Point(42, 157);
             btn_Actualizar.Name = "btn_Actualizar";
-            btn_Actualizar.Size = new Size(40, 40);
+            btn_Actualizar.Size = new Size(58, 53);
             btn_Actualizar.TabIndex = 7;
             btn_Actualizar.UseVisualStyleBackColor = false;
             btn_Actualizar.Click += btn_Actualizar_Click;
@@ -144,10 +139,9 @@
             // 
             btn_GeneraConector.BackgroundImage = (Image)resources.GetObject("btn_GeneraConector.BackgroundImage");
             btn_GeneraConector.BackgroundImageLayout = ImageLayout.Stretch;
-            btn_GeneraConector.Location = new Point(47, 233);
-            btn_GeneraConector.Margin = new Padding(3, 2, 3, 2);
+            btn_GeneraConector.Location = new Point(42, 311);
             btn_GeneraConector.Name = "btn_GeneraConector";
-            btn_GeneraConector.Size = new Size(40, 40);
+            btn_GeneraConector.Size = new Size(58, 54);
             btn_GeneraConector.TabIndex = 13;
             btn_GeneraConector.UseVisualStyleBackColor = true;
             btn_GeneraConector.Click += btn_GeneraConector_Click;
@@ -156,19 +150,19 @@
             // 
             lbl_GeneraConector.AutoSize = true;
             lbl_GeneraConector.BackColor = Color.Transparent;
-            lbl_GeneraConector.Location = new Point(93, 246);
+            lbl_GeneraConector.Location = new Point(106, 328);
             lbl_GeneraConector.Name = "lbl_GeneraConector";
-            lbl_GeneraConector.Size = new Size(141, 15);
+            lbl_GeneraConector.Size = new Size(178, 20);
             lbl_GeneraConector.TabIndex = 14;
             lbl_GeneraConector.Text = "Generar Conector Herraje";
             // 
             // Menu
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            BackgroundImageLayout = ImageLayout.None;
-            ClientSize = new Size(683, 354);
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(781, 472);
             Controls.Add(lbl_GeneraConector);
             Controls.Add(btn_GeneraConector);
             Controls.Add(btn_Actualizar);
@@ -181,7 +175,6 @@
             Controls.Add(btn_LoadXml);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(3, 2, 3, 2);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "Menu";
