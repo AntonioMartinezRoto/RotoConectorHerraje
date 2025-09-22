dcda37dba704da6ef39707a2ddc1d29eae9a47e9
Antonio Martínez <antonio.martinez@roto-frank.com>
2025-09-22 15:43:48 +0200

    Funcionalidad Combinar Conectores

---
+++ CombinarConectores.cs
@@ -0,0 +1,272 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace RotoConectorHerraje
+{
+    public partial class CombinarConectores : Form
+    {
+        private string ConnectionString { get; set; }
+        private string Server { get; set; }
+        private string BaseDatos { get; set; }
+
+        public CombinarConectores()
+        {
+            InitializeComponent();
+        }
+
+        public CombinarConectores(string ConnectionString, string Server, string BaseDatos)
+        {
+            InitializeComponent();
+            this.ConnectionString = ConnectionString;
+            this.Server = Server;   
+            this.BaseDatos = BaseDatos;
+        }
+
+        private void CombinarConectores_Load(object sender, EventArgs e)
+        {
+            CargarDatos();
+            lbl_Conexion.Text = Server + @"\" + BaseDatos;
+        }
+
+        private void CargarDatos()
+        {
+            List<string> conectoresList = new List<string>();
+            using SqlConnection conexion = new SqlConnection(ConnectionString);
+            conexion.Open();
+
+            using SqlCommand cmd = new SqlCommand("SELECT Codigo, XML FROM ConectorHerrajes", conexion);
+            using SqlDataReader reader = cmd.ExecuteReader();
+
+            conectoresList.Clear();
+
+            while (reader.Read())
+            {
+                conectoresList.Add(reader[0].ToString());
+            }
+
+            listBox_AllConectores.Items.Clear();
+            foreach (string conector in conectoresList.OrderBy(c => c))
+            {
+                listBox_AllConectores.Items.Add(conector);
+            }
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            if (listBox_AllConectores.SelectedItem != null)
+            {
+                var item = listBox_AllConectores.SelectedItem;
+                listBox_Combinar.Items.Add(item);
+                listBox_AllConectores.Items.Remove(item);
+            }
+        }
+
+        private void btn_Add_All_Click(object sender, EventArgs e)
+        {
+            foreach (var item in listBox_AllConectores.Items)
+            {
+                listBox_Combinar.Items.Add(item);
+            }
+            listBox_AllConectores.Items.Clear();
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            if (listBox_Combinar.SelectedItem != null)
+            {
+                var item = listBox_Combinar.SelectedItem;
+                listBox_AllConectores.Items.Add(item);
+                listBox_Combinar.Items.Remove(item);
+            }
+        }
+
+        private void btn_DeleteAll_Click(object sender, EventArgs e)
+        {
+            foreach (var item in listBox_Combinar.Items)
+            {
+                listBox_AllConectores.Items.Add(item);
+            }
+            listBox_Combinar.Items.Clear();
+        }
+
+        private Connector CargarConectorDesdeBaseDeDatos(string conectorName)
+        {
+            try
+            {
+                string xmlString = null;
+
+                using (var conexion = new SqlConnection(ConnectionString))
+                {
+                    conexion.Open();
+
+                    var query = "SELECT XML FROM ConectorHerrajes WHERE Codigo = @codigo";
+                    using var cmd = new SqlCommand(query, conexion);
+                    cmd.Parameters.AddWithValue("@codigo", conectorName);
+
+                    var result = cmd.ExecuteScalar();
+                    if (result != DBNull.Value && result != null)
+                    {
+                        xmlString = result.ToString();
+                    }
+                }
+
+                if (!string.IsNullOrWhiteSpace(xmlString))
+                {
+                    return DeserializarXML<Connector>(xmlString);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar el conector: " + conectorName);
+                return null;
+            }
+        }
+
+        private void btn_up_Click(object sender, EventArgs e)
+        {
+            if (listBox_Combinar.SelectedItem == null) return;
+
+            int index = listBox_Combinar.SelectedIndex;
+            if (index > 0) // no subir si ya está en la primera posición
+            {
+                var item = listBox_Combinar.SelectedItem;
+
+                listBox_Combinar.Items.RemoveAt(index);
+                listBox_Combinar.Items.Insert(index - 1, item);
+
+                listBox_Combinar.SelectedIndex = index - 1; // mantener selección
+            }
+        }
+
+        private void btn_down_Click(object sender, EventArgs e)
+        {
+            if (listBox_Combinar.SelectedItem == null) return;
+
+            int index = listBox_Combinar.SelectedIndex;
+            if (index < listBox_Combinar.Items.Count - 1) // no bajar si ya está en la última posición
+            {
+                var item = listBox_Combinar.SelectedItem;
+
+                listBox_Combinar.Items.RemoveAt(index);
+                listBox_Combinar.Items.Insert(index + 1, item);
+
+                listBox_Combinar.SelectedIndex = index + 1; // mantener selección
+            }
+        }
+
+        private T DeserializarXML<T>(string xml)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using var reader = new StringReader(xml);
+            return (T)serializer.Deserialize(reader);
+        }
+
+        private string SerializarXml<T>(T objeto)
+        {
+            using (var stringWriter = new System.IO.StringWriter())
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                serializer.Serialize(stringWriter, objeto);
+                return stringWriter.ToString();
+            }
+        }
+
+        private void GuardarConectorEnBD(string xml)
+        {
+            try
+            {
+                string sql = @"INSERT INTO ConectorHerrajes (DataVerId, Codigo, XML) VALUES (dbo.GetCurrentDVID(), @Codigo, @Xml);";
+                //EnableButtons(false);
+                if (ExisteConectorEnBD(txt_ConectorName.Text))
+                {
+                    if (MessageBox.Show("Existe un conector con el nombre seleccionado ¿Quieres sobreescribirlo?", "Conector existente", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        sql = @"UPDATE ConectorHerrajes SET XML = @Xml Where Codigo = @Codigo;";
+                    }
+                    else
+                    {
+                        return;
+                    }
+
+                }
+
+                if (chk_Predefinido.Checked && !String.IsNullOrEmpty(txt_ConectorName.Text))
+                {
+                    sql += @"UPDATE VARIABLESGLOBALES SET VALOR = '" + txt_ConectorName.Text + "' WHERE NOMBRE = 'Conector Herraje';";
+                }
+
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    command.Parameters.AddWithValue("@Codigo", txt_ConectorName.Text);
+                    command.Parameters.AddWithValue("@Xml", xml);
+                    command.ExecuteNonQuery();
+                }
+                MessageBox.Show("Conector insertado correctamente.");
+
+                //EnableButtons(true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error insertando conector: " + ex.Message);
+                //EnableButtons(true);
+            }
+        }
+
+        private bool ExisteConectorEnBD(string conectorName)
+        {
+            using SqlConnection conexion = new SqlConnection(ConnectionString);
+            conexion.Open();
+
+            using SqlCommand cmd = new SqlCommand("SELECT Count(*) FROM ConectorHerrajes WHERE Codigo = '" + conectorName + "'", conexion);
+            using SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                return Convert.ToInt32(reader[0].ToString()) > 0;
+
+            }
+            return false;
+        }
+
+        private void bnt_Guardar_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txt_ConectorName.Text))
+            {
+                MessageBox.Show("Asigna un nombre al Conector que se va a crear", "Nombre necesario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (listBox_Combinar.Items.Count > 0)
+            {
+                Connector conectorFinal = new Connector();
+                conectorFinal.Message = "true";
+                conectorFinal.Nodes = new List<ConnectorNode>();
+
+                foreach (string conector in listBox_Combinar.Items)
+                {
+                    Connector conectorFusion = CargarConectorDesdeBaseDeDatos(conector);
+                    conectorFinal.Nodes.AddRange(conectorFusion.Nodes);
+                }
+
+                string xml = SerializarXml(conectorFinal);
+                GuardarConectorEnBD(xml);
+            }
+        }
+    }
+}
--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -40,6 +40,8 @@
             btn_Actualizar = new Button();
             btn_GeneraConector = new Button();
             lbl_GeneraConector = new Label();
+            label3 = new Label();
+            btn_CombinarConectores = new Button();
             statusStrip1.SuspendLayout();
             SuspendLayout();
             // 
@@ -47,9 +49,10 @@
             // 
             btn_LoadXml.BackgroundImage = (Image)resources.GetObject("btn_LoadXml.BackgroundImage");
             btn_LoadXml.BackgroundImageLayout = ImageLayout.Stretch;
-            btn_LoadXml.Location = new Point(42, 72);
+            btn_LoadXml.Location = new Point(35, 34);
+            btn_LoadXml.Margin = new Padding(3, 2, 3, 2);
             btn_LoadXml.Name = "btn_LoadXml";
-            btn_LoadXml.Size = new Size(58, 56);
+            btn_LoadXml.Size = new Size(51, 42);
             btn_LoadXml.TabIndex = 0;
             btn_LoadXml.UseVisualStyleBackColor = true;
             btn_LoadXml.Click += btn_LoadXml_Click;
@@ -58,9 +61,9 @@
             // 
             lbl_Xml.BackColor = Color.Transparent;
             lbl_Xml.Font = new Font("Segoe UI", 8F);
-            lbl_Xml.Location = new Point(106, 91);
+            lbl_Xml.Location = new Point(91, 48);
             lbl_Xml.Name = "lbl_Xml";
-            lbl_Xml.Size = new Size(586, 46);
+            lbl_Xml.Size = new Size(513, 34);
             lbl_Xml.TabIndex = 1;
             lbl_Xml.Text = "Seleccionar XML";
             // 
@@ -69,9 +72,10 @@
             statusStrip1.BackColor = Color.Transparent;
             statusStrip1.ImageScalingSize = new Size(20, 20);
             statusStrip1.Items.AddRange(new ToolStripItem[] { lbl_Conexion });
-            statusStrip1.Location = new Point(0, 446);
+            statusStrip1.Location = new Point(0, 357);
             statusStrip1.Name = "statusStrip1";
-            statusStrip1.Size = new Size(781, 26);
+            statusStrip1.Padding = new Padding(1, 0, 12, 0);
+            statusStrip1.Size = new Size(683, 22);
             statusStrip1.SizingGrip = false;
             statusStrip1.TabIndex = 2;
             statusStrip1.Text = "statusStrip1";
@@ -79,16 +83,17 @@
             // lbl_Conexion
             // 
             lbl_Conexion.Name = "lbl_Conexion";
-            lbl_Conexion.Size = new Size(151, 20);
+            lbl_Conexion.Size = new Size(118, 17);
             lbl_Conexion.Text = "toolStripStatusLabel1";
             // 
             // cmb_Conectores
             // 
             cmb_Conectores.DropDownStyle = ComboBoxStyle.DropDownList;
             cmb_Conectores.FormattingEnabled = true;
-            cmb_Conectores.Location = new Point(308, 172);
+            cmb_Conectores.Location = new Point(249, 108);
+            cmb_Conectores.Margin = new Padding(3, 2, 3, 2);
             cmb_Conectores.Name = "cmb_Conectores";
-            cmb_Conectores.Size = new Size(186, 28);
+            cmb_Conectores.Size = new Size(163, 23);
             cmb_Conectores.TabIndex = 3;
             cmb_Conectores.SelectedValueChanged += cmb_Conectores_SelectedValueChanged;
             // 
@@ -96,9 +101,10 @@
             // 
             btn_SetsNoUtilizados.BackgroundImage = (Image)resources.GetObject("btn_SetsNoUtilizados.BackgroundImage");
             btn_SetsNoUtilizados.BackgroundImageLayout = ImageLayout.Stretch;
-            btn_SetsNoUtilizados.Location = new Point(42, 240);
+            btn_SetsNoUtilizados.Location = new Point(35, 160);
+            btn_SetsNoUtilizados.Margin = new Padding(3, 2, 3, 2);
             btn_SetsNoUtilizados.Name = "btn_SetsNoUtilizados";
-            btn_SetsNoUtilizados.Size = new Size(58, 51);
+            btn_SetsNoUtilizados.Size = new Size(51, 38);
             btn_SetsNoUtilizados.TabIndex = 4;
             btn_SetsNoUtilizados.UseVisualStyleBackColor = true;
             btn_SetsNoUtilizados.Click += btn_SetsNoUtilizados_Click;
@@ -107,9 +113,9 @@
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
-            label1.Location = new Point(106, 253);
+            label1.Location = new Point(91, 170);
             label1.Name = "label1";
-            label1.Size = new Size(116, 20);
+            label1.Size = new Size(91, 15);
             label1.TabIndex = 5;
             label1.Text = "Revisión de Sets";
             // 
@@ -117,9 +123,9 @@
             // 
             label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
-            label2.Location = new Point(106, 175);
+            label2.Location = new Point(91, 111);
             label2.Name = "label2";
-            label2.Size = new Size(149, 20);
+            label2.Size = new Size(119, 15);
             label2.TabIndex = 6;
             label2.Text = "Seleccionar Conector";
             // 
@@ -128,9 +134,10 @@
             btn_Actualizar.BackColor = Color.Transparent;
             btn_Actualizar.BackgroundImage = (Image)resources.GetObject("btn_Actualizar.BackgroundImage");
             btn_Actualizar.BackgroundImageLayout = ImageLayout.Stretch;
-            btn_Actualizar.Location = new Point(42, 157);
+            btn_Actualizar.Location = new Point(35, 98);
+            btn_Actualizar.Margin = new Padding(3, 2, 3, 2);
             btn_Actualizar.Name = "btn_Actualizar";
-            btn_Actualizar.Size = new Size(58, 53);
+            btn_Actualizar.Size = new Size(51, 40);
             btn_Actualizar.TabIndex = 7;
             btn_Actualizar.UseVisualStyleBackColor = false;
             btn_Actualizar.Click += btn_Actualizar_Click;
@@ -139,9 +146,10 @@
             // 
             btn_GeneraConector.BackgroundImage = (Image)resources.GetObject("btn_GeneraConector.BackgroundImage");
             btn_GeneraConector.BackgroundImageLayout = ImageLayout.Stretch;
-            btn_GeneraConector.Location = new Point(42, 311);
+            btn_GeneraConector.Location = new Point(35, 213);
+            btn_GeneraConector.Margin = new Padding(3, 2, 3, 2);
             btn_GeneraConector.Name = "btn_GeneraConector";
-            btn_GeneraConector.Size = new Size(58, 54);
+            btn_GeneraConector.Size = new Size(51, 40);
             btn_GeneraConector.TabIndex = 13;
             btn_GeneraConector.UseVisualStyleBackColor = true;
             btn_GeneraConector.Click += btn_GeneraConector_Click;
@@ -150,19 +158,43 @@
             // 
             lbl_GeneraConector.AutoSize = true;
             lbl_GeneraConector.BackColor = Color.Transparent;
-            lbl_GeneraConector.Location = new Point(106, 328);
+            lbl_GeneraConector.Location = new Point(91, 226);
             lbl_GeneraConector.Name = "lbl_GeneraConector";
-            lbl_GeneraConector.Size = new Size(178, 20);
+            lbl_GeneraConector.Size = new Size(141, 15);
             lbl_GeneraConector.TabIndex = 14;
             lbl_GeneraConector.Text = "Generar Conector Herraje";
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Location = new Point(91, 286);
+            label3.Name = "label3";
+            label3.Size = new Size(123, 15);
+            label3.TabIndex = 16;
+            label3.Text = "Combinar Conectores";
+            // 
+            // btn_CombinarConectores
+            // 
+            btn_CombinarConectores.BackgroundImage = (Image)resources.GetObject("btn_CombinarConectores.BackgroundImage");
+            btn_CombinarConectores.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_CombinarConectores.Location = new Point(35, 273);
+            btn_CombinarConectores.Margin = new Padding(3, 2, 3, 2);
+            btn_CombinarConectores.Name = "btn_CombinarConectores";
+            btn_CombinarConectores.Size = new Size(51, 40);
+            btn_CombinarConectores.TabIndex = 15;
+            btn_CombinarConectores.UseVisualStyleBackColor = true;
+            btn_CombinarConectores.Click += btn_CombinarConectores_Click;
+            // 
             // Menu
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(781, 472);
+            ClientSize = new Size(683, 379);
+            Controls.Add(label3);
+            Controls.Add(btn_CombinarConectores);
             Controls.Add(lbl_GeneraConector);
             Controls.Add(btn_GeneraConector);
             Controls.Add(btn_Actualizar);
@@ -175,6 +207,7 @@
             Controls.Add(btn_LoadXml);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 2, 3, 2);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "Menu";
@@ -200,5 +233,7 @@
         private Button btn_Actualizar;
         private Button btn_GeneraConector;
         private Label lbl_GeneraConector;
+        private Label label3;
+        private Button btn_CombinarConectores;
     }
 }
